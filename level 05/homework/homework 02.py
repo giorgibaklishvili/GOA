336e50f8b2cd79d78fb38f1fe7329cc774e736c3
giorgibaklishvili <giorgi.baklishvili@gmail.com>
2024-05-28 11:22:02 -0700

    commit

---
+++ level 05/homework/homework 02.py
@@ -0,0 +1,14 @@
+name = "giorgi"
+last_name = "baklishvili"
+country = "georgia"
+city = "gurjaani"
+age = "13"
+birthday = "august14"
+hobby  = "basketball"
+
+print("my name is:",name,last_name)
+print("i live in:",country,city)
+print("my age is:",age,"my birthday is on:",birthday)
+print("my hobby is:",hobby)
+
+
