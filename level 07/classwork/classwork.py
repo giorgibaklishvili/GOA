8ed5e2921f538ac840e4ee656f8d8285cb80cacf
giorgibaklishvili <giorgi.baklishvili@gmail.com>
2024-06-06 11:50:54 -0700

    commit

---
+++ level 07/classwork/classwork.py
@@ -0,0 +1,50 @@
+name = giorgi
+last_name = baklishvili
+age = 13
+country = georgia
+favorite_car = porche
+favorite_color = black
+favorite_number = 30
+
+
+
+
+
+
+
+# name = 'dato'
+# name = 'gio'
+# name = "nika'
+
+# print(name,name,name)
+
+
+# name = input("what is your name: ")
+# last_name = input("what is your last name: ")
+# age = input("how old are you: ")
+# country = input("whivh country do ypu live in: ")
+
+
+# age = input('enter ypur age')
+#print(type(age))
+
+
+# x = int(input('Enter X: '))
+# y = int(input('Enter Y: '))
+
+#print(x+y)
+
+#str()
+#int()
+
+# x = '250'
+#print(pype(int(x)))
+
+
+# name = 'luka'
+# age = '14'
+# salary = 5.2
+
+#print(type(name))
+#print(type(age))
+#print(type(salary))
---
+++ level 07/homework/homework.py
@@ -0,0 +1,69 @@
+name = "giorgi"
+last_name = "baklishvili"
+age = "13"
+favorite_car = "porche"
+country = "georgia"
+
+print(name +" "+ last_name +" "+ age +" "+ favorite_car +" "+ country)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+print(num1 + num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = 'dato'
+# name = 'gio'
+# NAME = 'nika'
+
+#print(name,name,NAME)
+
+
+# name = input("what is your name: ")
+# last_name = input("what is your last name: ")
+# age = input("how old are you: ")
+# country = input("what country do you live in: ")
+ 
+#print('My name is', name, last_name, 'i am' age, 'years old' 'and i live', country)
+
+
+# age = input('enter your age: ')
+# orint(type(age))
+
+
+# x = int(input('Enter x: '))
+# y = int(input('Enter Y: '))
+
+#print(x + y)
+
+#str()
+#int()
+
+
+# x = '250'
+# print(type(int(x)))
+
+
+# name = 'gega'
+# age = 14
+# salary = 5.2
+
+
+#print(type(name))
+#print(type(age))
+#print(type(salary))
